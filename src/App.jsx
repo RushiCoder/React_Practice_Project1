d7787cd8e84357ba366de8eacd41fcff55d5a8ce
Rushikesh Chincholkar <114005115+RushiCoder@users.noreply.github.com>
2023-07-08 23:36:22 +0530

    Add files via upload
    This is a Practice Project only

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import HeroSection from "./components/Hero";
+import Navigation from './components/Navigation';
+
+const App = () => {
+  return (
+   <div>
+     <Navigation/>
+     <HeroSection/>
+  </div>
+
+  );
+};
+export default App;
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,22 @@
+ const Navigation = () => {
+    return  (
+        <nav className="container">
+        <div className="logo">
+          <img src="/images/brand_logo.png" alt="logo" />
+        </div>
+        <ul>
+          <li href="#">Menu</li>
+          <li href="#">Location</li>
+          <li href="#">About</li>
+          <li href="#">Contact</li>
+        </ul>
+     
+      <button>
+        Login
+      </button>
+      </nav>
+
+    );
+ };
+
+ export default Navigation;
